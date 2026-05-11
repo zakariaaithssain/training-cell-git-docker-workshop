db10118ecc36a0494349649093cd8d23c9579128
MarwaX <it.inspairx@gmail.com>
2026-05-11 20:43:52 +0100

    ajout produit test

--- src/website/core/config.py
+++ src/website/core/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     app_name: str = "Workshop Marketplace"
     debug: bool = True
     database_url: str = "sqlite:///./workshop.db"
+    #database_url: str = "postgresql://postgres:postgremedic@localhost:5432/workshop_competition"
     
     # Sécurité pour la page des résultats : passer à True le jour J
     show_results: bool = False
--- src/website/shops/test.py
+++ src/website/shops/test.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+from website.core.models import Product
+
 router = APIRouter()
 shop_name = "testt shop"
 class ProductItem(BaseModel):
@@ -10,7 +12,9 @@ class ProductItem(BaseModel):
     image_filename: str
     stock: int = 10
 # les membres : ajoutez vos  produits ici
-inventory = []
+inventory = [
+        Product(name="Chreb o skotx2", price=57.00, category="Mug", stock=20, image_filename="sample3.jpg")
+]
 @router.get("/products")
 def get_products():
     return inventory
