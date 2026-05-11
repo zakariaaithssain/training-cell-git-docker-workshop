cc8bc2d5a56da37aa528145026d053c3f8e83d86
zakariaaithssain <zakariaithssain@gmail.com>
2026-05-11 19:18:16 +0100

    test test add product chrebt o skot

--- src/website/shops/test.py
+++ src/website/shops/test.py
@@ -1,16 +1,14 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+from website.core.models import Product
+
 router = APIRouter()
 shop_name = "testt shop"
-class ProductItem(BaseModel):
-    name: str
-    price: float
-    category: str
-    description: str
-    image_filename: str
-    stock: int = 10
+
 # les membres : ajoutez vos  produits ici
-inventory = []
+inventory: list[Product] = [
+    Product(name="Chreb o skot", price=55.00, category="Mug", stock=20, image_filename="sample3.jpg"),
+]
 @router.get("/products")
 def get_products():
     return inventory
